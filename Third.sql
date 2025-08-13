827356355e0f8ed8d3e770e9a1e926714224879c
Ankit Bhagat <bhagatankit502@gmail.com>
2025-08-13 19:49:09 +0530

    Initial commit with SQL files

--- Third.sql
+++ Third.sql
@@ -1,24 +1,36 @@
 SHOW DATABASES;
 USE startersql;
--- update users SET salary = 45000 WHERE id = 1;
+update users SET salary = 45000 WHERE id = 1;
 
--- UPDATE users SET name = 'Alicia', email = 'alicia@gmail.com' WHERE id  = 2;
+UPDATE users SET name = 'Alicia', email = 'alicia@gmail.com' WHERE id  = 2;
 
--- UPDATE users SET salary = 70000 WHERE id = 5 ;
+UPDATE users SET salary = 70000 WHERE id = 5 ;
 
--- UPDATE users SET name = 'Aisha Khan' where email = 'aisha@example.com';
+UPDATE users SET name = 'Aisha Khan' where email = 'aisha@example.com';
 
--- UPDATE users SET salary = salary + 10000 WHERE salary < 60000;
+UPDATE users SET salary = salary + 10000 WHERE salary < 60000;
 
--- select * from users;
+select * from users;
 
--- SET SQL_SAFE_UPDATES = 0;
+SET SQL_SAFE_UPDATES = 0;
 
--- DELETE FROM users
--- WHERE salary > 65000;
+DELETE FROM users
+WHERE salary > 65000;
 
--- DELETE FROM users WHERE id = 3;
+DELETE FROM users WHERE id = 3;
 
--- select * from users WHERE id = 10;
+select * from users WHERE id = 10;
 
-SELECT COUNT(*) FROM users;
+-- FUNCTIONS IN SQL  
+
+ COUNT, MAX, MIN, 
+
+SELECT COUNT(*) FROM users WHERE gender = 'Female'; 
+SELECT MIN(salary) AS min_salary, MAX(salary) AS max_salary FROM users;
+
+-- SUM, AVG 
+SELECT SUM(salary) AS total_salary FROM users;
+SELECT AVG(salary) AS avg_salary FROM users;
+
+-- GROUP BY
+SELECT gender , AVG(salary) AS avg_salary FROM users GROUP BY gender;
