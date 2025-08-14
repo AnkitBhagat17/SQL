3a87766b6b856b803752300b973cfacc0a4f4d23
Ankit Bhagat <bhagatankit502@gmail.com>
2025-08-14 21:09:31 +0530

    Initial commit with SQL files

--- Fourth.sql
+++ Fourth.sql
@@ -1,18 +1,14 @@
+
+CREATE DATABASE IF NOT EXISTS startersql;
 USE startersql;
-DROP TABLE IF EXISTS addresses;
 
-CREATE TABLE addresses (
-id INT AUTO_INCREMENT PRIMARY KEY,
-user_id INT,
-street VARCHAR(255),
-state VARCHAR(100),
-pincode VARCHAR(10),
-CONSTRAINT fk_user FOREIGN KEY(user_id) references users(id) ON DELETE CASCADE
+CREATE TABLE users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    gender ENUM('Male','Female','Other'),
+    date_of_birth DATE,
+    salary DECIMAL(10,2),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
 );
-
-INSERT INTO addresses (user_id, street , city, state, pincode)
-VALUES
-(1,'221 MG Road','Mumbai','Maharashtra','400001'),
-(22,'14  Park Street','Kolkata','West Bengal','700018'),
-(3,'32 Residency Road','Bengaluru','Karnataka','560025'),
-(24,
